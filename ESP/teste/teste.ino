d0553fe9fc2df7a97ea22e15bc88f3505c2e6ef5
Vandelson Cleitoso <julim.generic@gmail.com>
2024-09-20 20:41:53 -0300

    Arrumar permissões + ip estático

--- ESP/teste/teste.ino
+++ ESP/teste/teste.ino
@@ -4,35 +4,45 @@
 const char* ssid = "flavio-2G"; 
 const char* password = "2219280214"; 
 
-WebServer server(80); // Inicializa o servidor na porta 80
+IPAddress local_IP(192, 168, 137, 218);
+IPAddress gateway(192, 168, 137, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+WebServer server(80);
 
 void setup() {
-  Serial.begin(9600); 
-  server.sendHeader("Access-Control-Allow-Origin", "*"); // Permitir acesso de qualquer origem
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando ao Wi-Fi...");
-  }
-  Serial.println("Conectado ao Wi-Fi");
+    Serial.begin(9600);
+    delay(100); // Aguarde para garantir que o Serial esteja pronto
 
-  // Iniciar servidor
-  server.on("/", handleRoot);
-  server.on("/data", handleData);
-  server.begin();
+    if (!WiFi.config(local_IP, gateway, subnet)) {
+        Serial.println("Falha ao configurar IP estático");
+    } else {
+        Serial.println("IP estático configurado");
+    }
 
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Conectando ao Wi-Fi...");
+    }
+    Serial.println("Conectado ao Wi-Fi");
+
+    server.on("/", handleRoot);
+    server.on("/data", handleData);
+    server.begin();
 }
 
 void loop() {
-  //teste sem sensores
-   server.handleClient(); // Lidar com clientes HTTP
-
+    server.handleClient();
 }
+
 void handleRoot() {
-  server.send(200, "text/html", "<h1>Bem-vindo ao Sensor NPK</h1><p>Acesse <a href='/data'>/data</a> para ver os dados do sensor.</p>");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/html", "<h1>Bem-vindo ao Sensor NPK</h1><p>Acesse <a href='/data'>/data</a> para ver os dados do sensor.</p>");
 }
 
 void handleData() {
-  String valores_estaticos = "plants?n=1&p=1&k=1";
-  server.send(200, "text/plain", valores_estaticos);
+    server.sendHeader("Access-Control-Allow-Origin", "*"); 
+    String valores_estaticos = "plants?n=1&p=1&k=1";
+    server.send(200, "text/plain", valores_estaticos);
 }
