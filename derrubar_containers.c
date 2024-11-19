3f20f6e2c5543552f51804e3a23777809a5f418e
Vandelson Cleitoso <julim.generic@gmail.com>
2024-11-18 22:44:03 -0300

    Derubar containers
    script de derrubar containers arrumado

--- derrubar_containers.c
+++ derrubar_containers.c
@@ -4,8 +4,8 @@
 int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
 	printf("Pequeno script pra derrubar containers criados");
-	system("echo Derrubar container MySQL && cd Container && sudo docker-compose down -d");
-	system("echo derrubar  Elastic Search e Kibana && cd elasticsearch && sudo docker-compose down -d && cd .. && cd kibana && sudo docker-compose down -d"); 
+	system("echo Derrubar container MySQL && cd Container && sudo docker-compose down -v");
+	system("echo derrubar  Elastic Search e Kibana && cd elasticsearch && sudo docker-compose down -v && cd .. && cd kibana && sudo docker-compose down -v"); 
 	 
     return 0;
 }
